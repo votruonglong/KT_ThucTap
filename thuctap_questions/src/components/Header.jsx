0f6f521f3da4ede2f9721814a6986fb59378aee0
Dang Huynh <danghuynh1526@gmail.com>
2024-08-17 14:52:19 +0700

    push respnsove for screen

--- thuctap_questions/src/components/Header.jsx
+++ thuctap_questions/src/components/Header.jsx
@@ -137,58 +137,6 @@ const Header = () => {
                 )
 
             }
-
-            {/* <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src={logo} className="h-8" alt="Amazing Tech Logo" />
-                    </a>
-                    <div className='header-center'>
-                        <form>
-                            <input
-                                className='form-control'
-                                placeholder='search questions...' />
-                        </form>
-                    </div>
-                    <button data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
-                    <div className="hidden w-full" id="navbar-hamburger">
-                        <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-                            <li>
-                                <a href="#" className="hover-link-menu">
-                                    Support
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/admin" className="hover-link-menu">
-                                    <span
-                                        onClick={handleredirect_admin}
-                                    ><i className="fas fa-user-cog"></i>&nbsp;&nbsp;Admin</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/" className="hover-link-menu">
-                                    <span
-                                        onClick={handleredirect_user}
-                                    ><i className="fas fa-user"></i>&nbsp;&nbsp;Intern</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="hover-link-menu">
-                                    <i className="fas fa-sign-out-alt">&nbsp;&nbsp;</i>Logout
-
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav> */}
-
-
         </>
     )
 
