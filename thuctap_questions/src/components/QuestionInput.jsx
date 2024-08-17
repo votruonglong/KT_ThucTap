06da1a9f3cfa442d23ac60b819d17199b39cceb1
Trần Gia Vĩ <trangiavi2018@gmail.com>
2024-08-17 14:29:00 +0700

    udate input, button

--- thuctap_questions/src/components/QuestionInput.jsx
+++ thuctap_questions/src/components/QuestionInput.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const QuestionForm = () => {
+    const [question, setQuestion] = useState('');
+    const [err, setErr] = useState('');
+
+    const handleInput = (e) => {
+        const value = e.target.value;
+
+        if (value.length >= 100) {
+            setErr('Bạn đã nhập quá số lượng ký tự cho phép');
+        } else {
+            setErr('');
+        }
+
+        setQuestion(value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (question.length <= 100) {
+            console.log(question);
+            setErr('');
+        }
+    };
+
     return (
         <div>
-            <form id="questionForm">
+            <form id="questionForm" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     id="questionInput"
                     name="question"
-                    placeholder="Nhập câu hỏi của bạn"  
-                    // required
+                    placeholder="Nhập câu hỏi của bạn"
+                    maxLength={100}
+                    value={question}
+                    onChange={handleInput}
+                    required
                 />
-                <button id="submitButton" type="submit">Gửi</button>
+                <button className="button">
+                Gửi
+                </button>         
             </form>
+            {err && <p id="errorMessage">{err}</p>}
         </div>
     );
-}
+};
+
 export default QuestionForm;
+
